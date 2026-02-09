784755dace9f3b34a78eb4f3aa736ce89472f22d
KainKues <kainkues@gmail.com>
2026-02-09 22:21:30 +0300

    add buttons

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 "use strict"
 
+
+function Basic(){
+    // Переменные и типы данных
+    let name = "Anna";
+    let age = 25;
+    let isStudent = true;
+
+    console.log(name, typeof(name));
+    console.log(age, typeof(age));
+    console.log(isStudent, typeof(isStudent));
+
+    // Арифметика
+    let forMeal = 250;
+    let forTran = 100;
+
+    let inWeek = (forMeal+ forTran)*7;
+    let inMonth = (forMeal+ forTran)*30;
+
+    console.log('Пользователь тратит в неделю '+inWeek+" рублей");
+    console.log('Пользователь тратит в месяц '+inMonth+" рублей");
+
+    // Шаблонные строки
+    console.log("Привет, меня зовут "+name+" , мне "+age+" лет.");
+
+    // Логические операторы
+    age = 17;
+    if (age >= 18){
+        console.log(true);
+    }
+    else(console.log(false));
+}
+
 // Оценка знаний
 function getScore(){
     let score = prompt("Поставьте оценку от 1 до 5");
@@ -20,12 +52,18 @@ function getScore(){
     }
 }    
 
-// Тернарный оператор
+// Проверка пароля
 function logIn(){
     let password = "js2025";
     let input = prompt('Введите пароль');
     password == input ? alert("Доступ разрешен") : alert("Ошибка входа");
 }    
+
+// Тернарный оператор
+function saleTrue(){
+    let isStudent = true;
+    isStudent == true? alert('Скидка активирована'): alert('Без скидки');
+}
 
 // Счётчик чисел
 function getAllNum(){
@@ -186,30 +224,32 @@ function arrayfunction(){
     }
     console.log(array)
 
-function getMax(array){
+    function getMax(array){
     let max = 0;
     array.forEach(element => {
         if (element > max){
             return max = element;} 
     })
     console.log('Максимальный элемент - ' + max);
-}
-getMax(array);
+    }
+    getMax(array);
 }
 
 // Комбинированное задание
-array = [];
-array.push(Number(prompt('Введите число от 1 до 5')));
-array.push(Number(prompt('Введите число от 1 до 5')));
-array.push(Number(prompt('Введите число от 1 до 5')));
-console.log(array);
+function scoreCheck(){
+    array = [];
+    array.push(Number(prompt('Введите число от 1 до 5')));
+    array.push(Number(prompt('Введите число от 1 до 5')));
+    array.push(Number(prompt('Введите число от 1 до 5')));
+    console.log(array);
 
-let sum = array.reduce((acc, val)=>{
-        return acc + val;
-    })
-check = sum / array.length;    
-console.log("среднее арифметическое = " + check);
-if (check >= 4){
-    console.log('Молодец!');
-} 
-else (console.log('Подтянись!'));
+    let sum = array.reduce((acc, val)=>{
+            return acc + val;
+        })
+    check = sum / array.length;    
+    console.log("среднее арифметическое = " + check);
+    if (check >= 4){
+        console.log('Молодец!');
+    } 
+    else (console.log('Подтянись!'));
+}
